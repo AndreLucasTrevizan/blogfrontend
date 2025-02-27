07fca12025b80a66ceeae6747d5780197cabcc18
André Lucas Trevizan <trevizan.al@gmail.com>
2025-02-27 16:42:29 -0300

    Frontend

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: 'build',
+  env: {
+    apiUrl: 'https://elegant-achievement-production.up.railway.app'
+  }
 };
 
 export default nextConfig;
