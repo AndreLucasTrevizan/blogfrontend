4a12f43ce1ab9c386821b5da17c4e65235613b9b
André Lucas Trevizan <trevizan.al@gmail.com>
2025-02-27 14:15:52 -0300

    Adjusting middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,3 @@
-import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
@@ -9,32 +8,25 @@ export const config = {
     '/profile',
     '/my_posts',
     '/sign_in',
-    '/sign_up'
+    '/sign_up',
   ],
-}
-
-const authRoutes = [
-  '/',
-  '/posts/:path*',
-  '/profile',
-  '/my_posts',
-];
+};
 
 const notAuthRoutes = [
   '/sign_in',
   '/sign_up'
-]
- 
-export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-  const token = request.cookies.get("token");
+];
 
-  if (authRoutes.includes(pathname) && !token) {
-    return NextResponse.redirect(new URL('/sign_in', request.url));
-  }
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  const token = req.cookies.get("token");
   
   if (notAuthRoutes.includes(pathname) && token) {
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL('/', req.url));
+  }
+
+  if (!token && !notAuthRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL('/sign_in', req.url));
   }
 
   return NextResponse.next();
