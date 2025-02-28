f40d372fe68ab39df8edf5fb3965603a83914690
André Lucas Trevizan <trevizan.al@gmail.com>
2025-02-28 17:00:03 -0300

    Adjusting momentjs

--- src/app/_helper/date.ts
+++ src/app/_helper/date.ts
@@ -4,5 +4,5 @@ moment.locale(process.env.dateLang);
 
 export const getDateWithHour = (date: Date) => {
 
-  return moment(date).tz('America/Sao_Paulo').format('LLL');
+  return moment.tz(date, 'America/Sao_Paulo').format('DD/MM/yyyy HH:mm');
 }
